73c693bbe406edaad38f5096955e7b94f61e6407
CharlesGPT <chimecharles23@gmail.com>
2025-10-21 08:21:01 -0400

    Done with the hero section

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ searchIcon.onclick = () => {
 searchClose.onclick = () => {
     searchForm.classList.remove('active')
 }
+
+//Initialize swiper
+const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".home-slider", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 4500,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+});
